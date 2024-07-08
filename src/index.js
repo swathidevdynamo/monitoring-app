5959f5a8457c00e4caf1b58c8cee64f3a74a5c68
swathi_munnangi73 <146412104+swathidevdynamo@users.noreply.github.com>
2024-07-08 15:01:04 +0200

    removing unused imports

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import store from './redux/store';
 import App from './App';
 
 
 // After
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
-const root = createRoot(container); 
+const root = createRoot(container);
 
 
 root.render(
-  // <Provider store={store}>
   <div>
     <Router>
       <App />
     </Router>
-    </div>
-  // </Provider>
+  </div>
 );
